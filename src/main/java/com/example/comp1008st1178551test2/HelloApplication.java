377b58c1829d215164f2c06c1b7ff777d432278b
SpencerCloughley <spencercloughley@gmail.com>
2023-03-08 09:16:49 -0500

    Question 12 complete

--- src/main/java/com/example/comp1008st1178551test2/HelloApplication.java
+++ src/main/java/com/example/comp1008st1178551test2/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.comp1008st1178551test2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dealership-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Dealership");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Spencer's Dealership!");
+        stage.getIcons().add(new Image(HelloApplication.class.getResourceAsStream("images/car.png")));
         stage.setScene(scene);
         stage.show();
     }
