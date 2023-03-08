94faa2c571d0aac499918efce227929d24d6c1f9
SpencerCloughley <spencercloughley@gmail.com>
2023-03-08 08:25:48 -0500

    Question 2 complete

---
+++ src/main/java/com/example/comp1008st1178551test2/Car.java
@@ -0,0 +1,8 @@
+package com.example.comp1008st1178551test2;
+
+public class Car {
+    private String make;
+    private String model;
+    private double price;
+    private int year;
+}
---
+++ src/main/java/com/example/comp1008st1178551test2/Dealership.java
@@ -0,0 +1,7 @@
+package com.example.comp1008st1178551test2;
+
+import java.util.ArrayList;
+
+public class Dealership {
+    private ArrayList<Car> inventory;
+}
