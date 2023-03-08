088903d5810a898e5ad6f396d3edd7b6f737889f
SpencerCloughley <spencercloughley@gmail.com>
2023-03-08 08:38:18 -0500

    Question 6 complete

--- src/main/java/com/example/comp1008st1178551test2/Dealership.java
+++ src/main/java/com/example/comp1008st1178551test2/Dealership.java
@@ -4,4 +4,16 @@ import java.util.ArrayList;
 
 public class Dealership {
     private ArrayList<Car> inventory;
+
+    public Dealership(){
+        this.inventory=new ArrayList<Car>();
+    }
+
+    public void addCar(Car car){
+        inventory.add(car);
+    }
+
+    public ArrayList<Car> getInventory(){
+        return inventory;
+    }
 }
